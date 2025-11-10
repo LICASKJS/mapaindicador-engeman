107dc8a12bada3b567544eb61e36a9c9d579630e
Lucas Mateus <licasmateusdasilva25@gmail.com>
2025-11-10 10:22:46 -0300

    Add files via upload

---
+++ tmp-playwright.js
@@ -0,0 +1,16 @@
+const { chromium } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  page.on('console', (msg) => {
+    const type = msg.type();
+    console.log('[console]', type, msg.text());
+  });
+  page.on('pageerror', (error) => {
+    console.log('[pageerror]', error.message);
+  });
+  await page.goto('https://mapaindicador-engeman.vercel.app/analise.html', { waitUntil: 'networkidle' });
+  await page.waitForTimeout(5000);
+  await browser.close();
+})();
